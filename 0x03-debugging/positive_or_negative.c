42e32a024cc9761a7cc4cbab6d3ea1d9d5037f38
Blessing Nxumalo <blessrmn9709@gmail.com>
2024-02-19 05:26:46 -0800

    01

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+
+{
+if (i > o)
+{
+	printf("%d is positive\n", i);
+
+	elseif (i == 0)
+		printf("%d is zero\n", i);
+else
+	printf("%d is negative\n", i);
+}
